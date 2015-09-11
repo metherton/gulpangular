f3c876ab8d9945f1619f906786ba5f5ba1f0d553
metherton <martinetherton63@googlemail.com>
2015-09-11 22:36:07 +0200

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,82 @@
 // gulp
 var gulp = require('gulp');
-
-// plugins
+var $ = require('gulp-load-plugins')();
+var argv = require('yargs').argv;
 var connect = require('gulp-connect');
 
+var paths = gulp.paths = {
+    src: 'src',
+    dist: 'dist',
+    tmp: '.tmp',
+    e2e: 'test/e2e',
+    unit: 'test/unit',
+    deps: 'bower_components',
+    run: 'run'
+};
+
+var browserSync = require('browser-sync').create();
+
 gulp.task('connect', function () {
-  connect.server({
-    root: 'app/',
-    port: 8888
-  });
+      connect.server({
+            root: 'app/',
+            port: 3000
+     });
 });
 
-var protractor = require("gulp-protractor").protractor;
+// Downloads the selenium webdriver
+gulp.task('webdriver-update', $.protractor.webdriver_update);
+gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
 
-gulp.task('e2e', function() {
-
-    return gulp.src(["./test/e2e/**/*.js"])
-        .pipe(protractor({
-            configFile: "test/e2e/protractor.conf.js",
-            args: ['--baseUrl', 'http://localhost:8888']
+function runProtractor(done) {
+    gulp.src(paths.e2e + '/**/*.js')
+        .pipe($.protractor.protractor({
+            configFile: 'test/e2e/protractor.conf.js',
+            //args: ['--baseUrl', 'http://localhost:3000']
         }))
-        .on('error', function(e) { throw e });
+        .on('error', function (err) {
+            // Make sure failed tests cause gulp to exit non-zero
+            throw err;
+        })
+        .on('end', function () {
+            // Close browser sync server
+            browserSync.exit();
+            done();
+        });
+}
 
+gulp.task('e2e', ['connect','webdriver-update'], runProtractor);
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
 });
+
+gulp.task('run', function () {
+    //var routes = {
+    //    '/bower_components': 'bower_components'
+    //};
+    browserSync.init({
+        server: {
+            baseDir: ["./app", 'run', 'bower_components', 'src'],
+         //   routes: routes
+        },
+        //port: argv.port || 3000
+    });
+});
+
+
+//gulp.task('e2e', function() {
+//
+//    return gulp.src(["./test/e2e/**/*.js"])
+//        .pipe(protractor({
+//            configFile: "test/e2e/protractor.conf.js",
+//            args: ['--baseUrl', 'http://localhost:8888']
+//        }))
+//        .on('error', function(e) { throw e });
+//
+//});
 
